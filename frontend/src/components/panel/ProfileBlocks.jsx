aa93f6c7b98d4c38552e6c3a3a93bffe12f16b29
FreQll <shchepin.man@gmail.com>
2024-03-13 12:47:06 +0200

    hot fix

--- frontend/src/components/panel/ProfileBlocks.jsx
+++ frontend/src/components/panel/ProfileBlocks.jsx
@@ -4,10 +4,10 @@ import { Separator } from "../ui/separator";
 import { logout } from "@/redux/actions/authActions";
 import { useNavigate } from "react-router-dom";
 import { EditProfile } from "./EditProfile";
-import InviteUsers from './InviteUsers';
-import { Dialog, DialogTrigger } from '../ui/dialog';
+import InviteUsers from "./InviteUsers";
+import { Dialog, DialogTrigger } from "../ui/dialog";
 
-const ProfileBlocks = ({ isSettingsOpen, setIsSettingsOpen }{ calendars }) => {
+const ProfileBlocks = ({ isSettingsOpen, setIsSettingsOpen, calendars }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -34,20 +34,23 @@ const ProfileBlocks = ({ isSettingsOpen, setIsSettingsOpen }{ calendars }) => {
             Edit profile
           </div>
           <Dialog>
-              <DialogTrigger asChild>
+            <DialogTrigger asChild>
+              <div className="p-[5px] rounded-[5px] text-[15px] hover:bg-[#00000011]">
+                Invite users
               </div>
-        <div className="p-[5px] rounded-[5px] text-[15px] hover:bg-[#00000011] cursor-pointer">
-          Invite users
-        </div>
-              </DialogTrigger>
-              <InviteUsers calendars={calendars} />
+              <div className="p-[5px] rounded-[5px] text-[15px] hover:bg-[#00000011] cursor-pointer">
+                Invite users
+              </div>
+            </DialogTrigger>
+            <InviteUsers calendars={calendars} />
           </Dialog>
-        <Separator className="bg-black opacity-20" />
-        <div
-          className="p-[5px] rounded-[5px] text-[15px] hover:bg-[#00000011]"
-          onClick={handleLogout}
-        >
-          Logout
+          <Separator className="bg-black opacity-20" />
+          <div
+            className="p-[5px] rounded-[5px] text-[15px] hover:bg-[#00000011]"
+            onClick={handleLogout}
+          >
+            Logout
+          </div>
         </div>
       </div>
     </div>
