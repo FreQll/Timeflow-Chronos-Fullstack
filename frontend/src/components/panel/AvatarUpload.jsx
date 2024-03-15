7a4f5b7e59541e5e1921368f96f9fa82526caa17
FreQll <shchepin.man@gmail.com>
2024-03-15 19:46:47 +0200

    button color fix

--- frontend/src/components/panel/AvatarUpload.jsx
+++ frontend/src/components/panel/AvatarUpload.jsx
@@ -27,7 +27,7 @@ export const AvatarUpload = () => {
           />
         </div>
         <label className="cursor-pointer mt-6">
-          <span className="mt-2 text-sm leading-normal px-4 py-2 bg-blue-600 text-white rounded-md font-bold">
+          <span className="mt-2 text-sm leading-normal px-4 py-2 bg-indigo-600 hover:bg-indigo-500 transition-colors text-white rounded-md font-bold">
             Select Avatar
           </span>
           <input
